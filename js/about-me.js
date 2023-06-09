053bbf79caf9dd4c26eb834c4fa463d2ed953391
Benjamin Fang <benjaminfang.ol@outlook.com>
2023-06-09 16:54:30 +0800

    add resume css

--- js/about-me.js
+++ js/about-me.js
@@ -6,5 +6,15 @@ function change_css_to_cv() {
 }
 
 
+function change_css_to_resume() {
+    const my_css_link = document.querySelector("#about-me-css");
+    my_css_link.setAttribute("href", "css/about-me-resume.css");
+    const my_photo = document.querySelector("#myphoto img");
+    my_photo.setAttribute("src", "imgs/fanghl_1inch.jpg");
+}
+
+
 let print_cv_but = document.getElementById("print-cv-button");
 print_cv_but.addEventListener("click", change_css_to_cv);
+let print_resume_but = document.getElementById("print-resume-button");
+print_resume_but.addEventListener("click", change_css_to_resume);
