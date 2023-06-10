2697ce5bbe6b9cdb6277ff4ed29b1c864de159ef
Benjamin Fang <benjaminfang.ol@outlook.com>
2023-06-10 14:25:07 +0800

    separate js code into a stand alone js file

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let mytime = document.getElementById("mytime");
+let mylabel = document.getElementById("timelabel");
+let birthday = new Date(1992, 9, 3);
+let preend = new Date(2064, 9, 4);
+let now_dt = new Date();
+
+let passed = (now_dt - birthday)/(1000 * 3600 * 24);
+let left = (preend - now_dt)/(1000 * 3600 * 24);
+
+mytime.max = passed + left;
+mytime.value = left;
+
+mylabel.textContent = "My Time " + "(" + left.toFixed(0) + "|" + passed.toFixed(0) + ")";
